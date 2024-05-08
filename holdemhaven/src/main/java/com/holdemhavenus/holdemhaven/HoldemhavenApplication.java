49971f23417d5b2f7f822b45918e06d9dc74aa67
AldoMunoz <aldothenal@gmail.com>
2024-05-08 11:51:13 -0400

    -created Java Spring Boot project via the initializer. -added required dependencies to pom.xml

---
+++ holdemhaven/src/main/java/com/holdemhavenus/holdemhaven/HoldemhavenApplication.java
@@ -0,0 +1,13 @@
+package com.holdemhavenus.holdemhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldemhavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoldemhavenApplication.class, args);
+    }
+
+}
---
+++ holdemhaven/src/test/java/com/holdemhavenus/holdemhaven/HoldemhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holdemhavenus.holdemhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldemhavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
